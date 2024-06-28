e7329529190b4df1d9fc49f627651d6c55d34b9e
tomohiro_0225 <express22212270@gmail.com>
2024-06-28 10:42:57 +0900

    @propsを使った初期値の設定

--- umarche_test/resources/views/components/tests/card.blade.php
+++ umarche_test/resources/views/components/tests/card.blade.php
@@ -1,3 +1,9 @@
+@props([
+    'title',
+    'content' => 'コンテントも何も入っていないです。',
+    'message' => '本文に何も入っていないです',
+])
+
 <div class="border-2 shadow-md w-1/4 p-2 mt-10">
     <div>{{ $title }}</div>
     <div>画像</div>
--- umarche_test/resources/views/tests/component-test1.blade.php
+++ umarche_test/resources/views/tests/component-test1.blade.php
@@ -2,5 +2,6 @@
     <x-slot name="header">ヘッダー1</x-slot>
     <p class="text-red-500 w-auto mx-auto">コンポーネントテスト1</p>
 
-    <x-tests.card title="タイトル" content="$contentに何かがいる" :message="$message"></x-tests.card>
+    <x-tests.card title="タイトル1" content="$contentに何かがいる" :message="$message"></x-tests.card>
+    <x-tests.card title="タイトル2"></x-tests.card>
 </x-tests.app>
