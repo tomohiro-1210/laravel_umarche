eaf377f0be1389990a6d1e068dd142a12b3e3452
tomohiro_0225 <express22212270@gmail.com>
2024-06-28 10:26:28 +0900

    属性で設定

---
+++ umarche_test/resources/views/components/tests/card.blade.php
@@ -0,0 +1,15 @@
+<div class="border-2 shadow-md w-1/4 p-2 mt-10">
+    <div>{{ $title }}</div>
+    <div>画像</div>
+    <div>{{ $content }}</div>
+</div>
+<div class="border-2 shadow-md w-1/4 p-2 mt-10">
+    <div>{{ $title }}</div>
+    <div>画像</div>
+    <div>{{ $content }}</div>
+</div>
+<div class="border-2 shadow-md w-1/4 p-2 mt-10">
+    <div>{{ $title }}</div>
+    <div>画像</div>
+    <div>{{ $content }}</div>
+</div>
--- umarche_test/resources/views/tests/component-test1.blade.php
+++ umarche_test/resources/views/tests/component-test1.blade.php
@@ -1,4 +1,6 @@
 <x-tests.app>
     <x-slot name="header">ヘッダー1</x-slot>
     <p class="text-red-500 w-auto mx-auto">コンポーネントテスト1</p>
+
+    <x-tests.card title="タイトル" content="$contentに何かがいる"></x-tests.card>
 </x-tests.app>
