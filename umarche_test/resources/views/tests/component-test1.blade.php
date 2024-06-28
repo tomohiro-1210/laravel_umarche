5d9824780030dc2b91b949ee13890486e74702e5
tomohiro_0225 <express22212270@gmail.com>
2024-06-28 10:20:10 +0900

    ヘッダーの名前付きスロットを追加

--- umarche_test/resources/views/tests/component-test1.blade.php
+++ umarche_test/resources/views/tests/component-test1.blade.php
@@ -1,3 +1,4 @@
 <x-tests.app>
-<p class="text-red-500 w-auto mx-auto">コンポーネントテスト1</p>
+    <x-slot name="header">ヘッダー1</x-slot>
+    <p class="text-red-500 w-auto mx-auto">コンポーネントテスト1</p>
 </x-tests.app>
--- umarche_test/resources/views/tests/component-test2.blade.php
+++ umarche_test/resources/views/tests/component-test2.blade.php
@@ -1,3 +1,4 @@
 <x-tests.app>
+<x-slot name="header">ヘッダー2</x-slot>
 <p class="text-green-500 my-2 mx-2 px-2 py-2">コンポーネントテスト2</p>
 </x-tests.app>
