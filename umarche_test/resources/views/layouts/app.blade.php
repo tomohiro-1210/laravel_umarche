316b26b6d24640a0edc2231ff851373fbabf7bab
tomohiro_0225 <express22212270@gmail.com>
2024-07-02 11:34:34 +0900

    ユーザー・オーナー・お客様それぞれのログイン画面設定。（マルチログイン）

--- umarche_test/resources/views/layouts/app.blade.php
+++ umarche_test/resources/views/layouts/app.blade.php
@@ -18,7 +18,12 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+            @if(auth('admin')->user())
+                @include('layouts.admin-navigation')
+            @elseif(auth('owners')->user())
+                @include('layouts.owner-navigation')
+            @elseif(auth('users')->user())
+                @include('layouts.user-navigation')
 
             <!-- Page Heading -->
             <header class="bg-white shadow">
