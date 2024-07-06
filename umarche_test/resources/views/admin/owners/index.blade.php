d5f98e2c850835f157f186dde67c8931859be9bb
tomohiro_0225 <express22212270@gmail.com>
2024-07-06 11:29:52 +0900

    routeやviewを編集

--- umarche_test/resources/views/admin/owners/index.blade.php
+++ umarche_test/resources/views/admin/owners/index.blade.php
@@ -8,9 +8,9 @@
     <div class="py-12">
         <div class="max-w-8xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-blue-200">
+                <div class="md:p-6 bg-white border-b border-blue-200">
                     <section class="text-blue-600 body-font">
-                        <div class="container px-5 py-12 mx-auto">
+                        <div class="container md:px-5 py-12 mx-auto">
                             <x-flash-message status="{{session('status')}}" />
                             <div class="flex flex-col text-center w-full mb-10">
                                 <h1 class="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">オーナー一覧</h1>
